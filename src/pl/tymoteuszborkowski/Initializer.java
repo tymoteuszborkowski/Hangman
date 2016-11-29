3986d4f4e7d108b78971777d8d9798e7b6dc595c
Tymek <tymoteuszborkowski@gmail.com>
2016-11-29 21:56:50 +0100

    Init
    init commit, ui, first project structure

---
+++ src/pl/tymoteuszborkowski/Initializer.java
@@ -0,0 +1,40 @@
+package pl.tymoteuszborkowski;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import pl.tymoteuszborkowski.ui.Buttons;
+
+public class Initializer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final GridPane layout = new GridPane();
+        final Buttons buttons = new Buttons();
+
+        final Button generateWordButton = buttons.generateWordButton();
+        final Button[] keyboardButtons = buttons.addKeyboardButtons();
+
+
+        layout.getChildren().addAll(keyboardButtons);
+        layout.getChildren().addAll(generateWordButton);
+        layout.setPadding(new Insets(10));
+        layout.setVgap(10);
+        layout.setHgap(10);
+
+    Scene mainScene = new Scene(layout, 450, 750);
+        layout.setAlignment(Pos.BASELINE_CENTER);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Hangman");
+        primaryStage.show();
+}
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/pl/tymoteuszborkowski/ui/Buttons.java
@@ -0,0 +1,54 @@
+package pl.tymoteuszborkowski.ui;
+
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class Buttons {
+
+    public Button[] addKeyboardButtons(){
+        final String[] keyboard = getLetters();
+        final Button[] buttons = new Button[keyboard.length];
+
+        for(int i = 0; i < buttons.length; i++){
+            buttons[i] = new Button();
+            buttons[i].setText(keyboard[i]);
+        }
+
+        for (int i = 0x0, countRow = 0x0, countColumn = 0x39; i < buttons.length; i++) {
+
+            GridPane.setConstraints(buttons[i], countRow, countColumn);
+            countRow++;
+            if(countRow == 9){
+                countRow = 0;
+                countColumn++;
+            }
+        }
+
+        return buttons;
+    }
+
+    public Button generateWordButton(){
+        final Button wordButton = new Button("Generate word");
+        wordButton.setMinSize(50, 30);
+        GridPane.setConstraints(wordButton, 3, 1, 3, 1, HPos.CENTER, VPos.CENTER);
+        return wordButton;
+    }
+
+
+
+
+
+
+    private static String[] getLetters(){
+        String[] keyboard = new String[26];
+
+        for(int i = 0x41; i <= 0x5A; i++){
+            keyboard[i-0x41] = "" + (char) i;
+        }
+
+        return keyboard;
+    }
+
+}
